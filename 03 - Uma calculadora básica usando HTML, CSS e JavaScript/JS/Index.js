ead613bb257c2398995ac7912e966032f0cf1ee5
Yuri Gabriel <yurixgabriel@gmail.com>
2023-09-19 16:37:49 -0300

    delete - js

--- 03 - Uma calculadora básica usando HTML, CSS e JavaScript/JS/Index.js
+++ 03 - Uma calculadora básica usando HTML, CSS e JavaScript/JS/Index.js
@@ -4,7 +4,18 @@ function clearDisplay() {
     display.value = "";
 }
 
-function numbers(number) {
-    var valor = number.textContent;
+function dellete() {
+    if (display.value != "")
+    {
+        if(display.value == "0") {
+            clearDisplay();
+        }else{
+            display.value = display.value.substring(0,display.value.length-1);
+        };
+    };
+}
+
+function elementos(elemento) {
+    var valor = elemento.textContent;
     display.value += valor;
 }
