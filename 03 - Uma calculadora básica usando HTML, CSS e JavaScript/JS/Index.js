b11ce5bf575aeb97d28cd099a1a8972c5c0249b1
Yuri Gabriel <yurixgabriel@gmail.com>
2023-09-19 17:08:53 -0300

    parenteses - js

--- 03 - Uma calculadora básica usando HTML, CSS e JavaScript/JS/Index.js
+++ 03 - Uma calculadora básica usando HTML, CSS e JavaScript/JS/Index.js
@@ -16,6 +16,31 @@ function dellete() {
 }
 
 function elementos(elemento) {
-    var valor = elemento.textContent;
+    var valor = elemento.value;
     display.value += valor;
+}
+
+function parenteses() {
+    var carac = display.value.length;
+    var a = 0;
+    var f = 0;
+
+    for(var i=0; i < carac; i++) {
+        
+        if(display.value[i] === "(") {
+            a++;
+        } else {
+            if(display.value[i] === ")") {
+                f++;
+            }
+        }
+    }
+
+    if(a == 0 || a == f) {
+        display.value += "(";
+    } else {
+        if(a == 1 || a > f) {
+            display.value += ")";
+        }
+    }
 }
