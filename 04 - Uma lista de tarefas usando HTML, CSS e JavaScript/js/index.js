d5b3dfcc46280b51f1667690653459e790b35af8
Yuri Gabriel <yurixgabriel@gmail.com>
2023-10-21 16:03:59 -0300

    TaskDesk - js (pronto)

--- 04 - Uma lista de tarefas usando HTML, CSS e JavaScript/js/index.js
+++ 04 - Uma lista de tarefas usando HTML, CSS e JavaScript/js/index.js
@@ -1,8 +1,17 @@
 function TaskDesk(task) {
     const taskDesk = document.getElementById("detalhesTarefa");
+    var taskTitle = document.getElementById("taskTitle");
+    var taskDescription = document.getElementById("taskDescription");
     
     if(taskDesk.style.display == "none") {
         taskDesk.style.display = "flex";
+        
+        var desk = task.querySelector(".tarefaDesk");
+        var h4 = desk.querySelector("h4");
+        var p = desk.querySelector("div p")
+
+        taskTitle.textContent = h4.textContent;
+        taskDescription.textContent = p.textContent;
     } else {
         taskDesk.style.display = "none";
     }
