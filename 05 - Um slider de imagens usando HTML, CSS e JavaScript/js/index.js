cf32968f80c00cf95800305551e974e5455e39e0
Yuri Gabriel <yurixgabriel@gmail.com>
2023-11-07 17:38:38 -0300

    estrutura base (remodelação simplificada) - html, css e js

---
+++ 05 - Um slider de imagens usando HTML, CSS e JavaScript/js/index.js
@@ -0,0 +1,29 @@
+const slide = document.getElementById("S1");
+
+function prev() {
+    if(slide.style.marginLeft == "0%") {
+        slide.style.marginLeft = "-400%";
+    } else if(slide.style.marginLeft == "-400%") {
+        slide.style.marginLeft = "-300%";
+    } else if(slide.style.marginLeft == "-300%") {
+        slide.style.marginLeft = "-200%";
+    } else if(slide.style.marginLeft == "-200%") {
+        slide.style.marginLeft = "-100%";
+    } else if(slide.style.marginLeft == "-100%") {
+        slide.style.marginLeft = "0%";
+    }
+}
+
+function next() {
+    if(slide.style.marginLeft == "0%") {
+        slide.style.marginLeft = "-100%";
+    } else if(slide.style.marginLeft == "-100%") {
+        slide.style.marginLeft = "-200%";
+    } else if(slide.style.marginLeft == "-200%") {
+        slide.style.marginLeft = "-300%";
+    } else if(slide.style.marginLeft == "-300%") {
+        slide.style.marginLeft = "-400%";
+    } else if(slide.style.marginLeft == "-400%") {
+        slide.style.marginLeft = "0%";
+    }
+}
