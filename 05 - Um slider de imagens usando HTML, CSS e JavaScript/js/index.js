908e2a00096a70dedf64b433a9f528f1096ddec5
Yuri Gabriel <yurixgabriel@gmail.com>
2023-11-07 19:13:35 -0300

    estruturação - final

--- 05 - Um slider de imagens usando HTML, CSS e JavaScript/js/index.js
+++ 05 - Um slider de imagens usando HTML, CSS e JavaScript/js/index.js
@@ -1,4 +1,5 @@
 const slide = document.getElementById("S1");
+var intervalID = setInterval(sliderAutomatic, 3000);
 
 function prev() {
     if(slide.style.marginLeft == "0%") {
@@ -12,6 +13,9 @@ function prev() {
     } else if(slide.style.marginLeft == "-100%") {
         slide.style.marginLeft = "0%";
     }
+
+    clearInterval(intervalID);
+    intervalID = setInterval(sliderAutomatic, 5000);
 }
 
 function next() {
@@ -26,4 +30,25 @@ function next() {
     } else if(slide.style.marginLeft == "-400%") {
         slide.style.marginLeft = "0%";
     }
+
+    clearInterval(intervalID);
+    intervalID = setInterval(sliderAutomatic, 5000);
+}
+
+function sliderAutomatic() {
+    if(slide.style.marginLeft == "0%") {
+    slide.style.marginLeft = "-100%";
+    } else {
+    if(slide.style.marginLeft == "-100%") {
+    slide.style.marginLeft = "-200%";
+    } else {
+    if(slide.style.marginLeft == "-200%") {
+    slide.style.marginLeft = "-300%";
+    } else {
+    if(slide.style.marginLeft == "-300%") {
+    slide.style.marginLeft = "-400%";
+    } else {
+    if(slide.style.marginLeft == "-400%") {
+    slide.style.marginLeft = "0%";
+    }}}}}
 }
