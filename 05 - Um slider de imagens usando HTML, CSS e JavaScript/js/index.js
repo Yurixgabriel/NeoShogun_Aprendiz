0a2ebc9aff77e3c24a2d130a6e3701c7179977e5
Yuri Gabriel <yurixgabriel@gmail.com>
2023-11-07 21:45:43 -0300

    elementos e sistema de controle + automatico - finalizado

--- 05 - Um slider de imagens usando HTML, CSS e JavaScript/js/index.js
+++ 05 - Um slider de imagens usando HTML, CSS e JavaScript/js/index.js
@@ -1,54 +1,154 @@
-const slide = document.getElementById("S1");
-var intervalID = setInterval(sliderAutomatic, 3000);
+var slide = document.querySelectorAll(".slide");
+var desk = document.querySelectorAll(".desk");
+var intervalID = setInterval(sliderAutomatic, 4000);
+var S = 0;
+var D = 0;
 
 function prev() {
-    if(slide.style.marginLeft == "0%") {
-        slide.style.marginLeft = "-400%";
-    } else if(slide.style.marginLeft == "-400%") {
-        slide.style.marginLeft = "-300%";
-    } else if(slide.style.marginLeft == "-300%") {
-        slide.style.marginLeft = "-200%";
-    } else if(slide.style.marginLeft == "-200%") {
-        slide.style.marginLeft = "-100%";
-    } else if(slide.style.marginLeft == "-100%") {
-        slide.style.marginLeft = "0%";
+    if(S == 0) {
+        clearInterval(intervalID);
+        for (let i=0; i < slide.length; i++) {
+            slide[i].style.display="none";
+            desk[i].style.display="none";
+        }
+        S = slide.length - 1;
+        slide[S].style.display="block";
+        desk[S].style.display="flex";
+    } else if(S == 4) {
+        clearInterval(intervalID);
+        for (let i=0; i < slide.length; i++) {
+            desk[i].style.display="none";
+            slide[i].style.display="none";
+        }
+        S = slide.length - 2;
+        desk[S].style.display="flex";
+        slide[S].style.display="block";
+    } else if(S == 3) {
+        clearInterval(intervalID);
+        for (let i=0; i < slide.length; i++) {
+            desk[i].style.display="none";
+            slide[i].style.display="none";
+        }
+        S = slide.length - 3;
+        desk[S].style.display="flex";
+        slide[S].style.display="block";
+    } else if(S == 2) {
+        clearInterval(intervalID);
+        for (let i=0; i < slide.length; i++) {
+            desk[i].style.display="none";
+            slide[i].style.display="none";
+        }
+        S = slide.length - 4;
+        desk[S].style.display="flex";
+        slide[S].style.display="block";
+    } else if(S == 1) {
+        clearInterval(intervalID);
+        for (let i=0; i < slide.length; i++) {
+            desk[i].style.display="none";
+            slide[i].style.display="none";
+        }
+        S = 0;
+        desk[S].style.display="flex";
+        slide[S].style.display="block";
     }
 
-    clearInterval(intervalID);
-    intervalID = setInterval(sliderAutomatic, 5000);
+    intervalID = setInterval(sliderAutomatic, 6000);
 }
 
 function next() {
-    if(slide.style.marginLeft == "0%") {
-        slide.style.marginLeft = "-100%";
-    } else if(slide.style.marginLeft == "-100%") {
-        slide.style.marginLeft = "-200%";
-    } else if(slide.style.marginLeft == "-200%") {
-        slide.style.marginLeft = "-300%";
-    } else if(slide.style.marginLeft == "-300%") {
-        slide.style.marginLeft = "-400%";
-    } else if(slide.style.marginLeft == "-400%") {
-        slide.style.marginLeft = "0%";
+    if(S == 0) {
+        clearInterval(intervalID);
+        for (let i=0; i < slide.length; i++) {
+            desk[i].style.display="none";
+            slide[i].style.display="none";
+        }
+        S++;
+        desk[S].style.display="flex";
+        slide[S].style.display="block";
+    } else if(S == 1) {
+        clearInterval(intervalID);
+        for (let i=0; i < slide.length; i++) {
+            desk[i].style.display="none";
+            slide[i].style.display="none";
+        }
+        S++;
+        desk[S].style.display="flex";
+        slide[S].style.display="block";
+    } else if(S == 2) {
+        clearInterval(intervalID);
+        for (let i=0; i < slide.length; i++) {
+            desk[i].style.display="none";
+            slide[i].style.display="none";
+        }
+        S++;
+        desk[S].style.display="flex";
+        slide[S].style.display="block";
+    } else if(S == 3) {
+        clearInterval(intervalID);
+        for (let i=0; i < slide.length; i++) {
+            desk[i].style.display="none";
+            slide[i].style.display="none";
+        }
+        S++;
+        desk[S].style.display="flex";
+        slide[S].style.display="block";
+    } else if(S == 4) {
+        clearInterval(intervalID);
+        for (let i=0; i < slide.length; i++) {
+            desk[i].style.display="none";
+            slide[i].style.display="none";
+        }
+        S = 0;
+        desk[S].style.display="flex";
+        slide[S].style.display="block";
     }
 
-    clearInterval(intervalID);
-    intervalID = setInterval(sliderAutomatic, 5000);
+    intervalID = setInterval(sliderAutomatic, 6000);
 }
 
 function sliderAutomatic() {
-    if(slide.style.marginLeft == "0%") {
-    slide.style.marginLeft = "-100%";
-    } else {
-    if(slide.style.marginLeft == "-100%") {
-    slide.style.marginLeft = "-200%";
-    } else {
-    if(slide.style.marginLeft == "-200%") {
-    slide.style.marginLeft = "-300%";
-    } else {
-    if(slide.style.marginLeft == "-300%") {
-    slide.style.marginLeft = "-400%";
-    } else {
-    if(slide.style.marginLeft == "-400%") {
-    slide.style.marginLeft = "0%";
-    }}}}}
+    if(S == 0) {
+        for (let i=0; i < slide.length; i++) {
+            desk[i].style.display="none";
+            slide[i].style.display="none";
+        }
+        S++;
+        desk[S].style.display="flex";
+        slide[S].style.display="block";
+    } else if(S == 1) {
+        for (let i=0; i < slide.length; i++) {
+            desk[i].style.display="none";
+            slide[i].style.display="none";
+        }
+        S++;
+        desk[S].style.display="flex";
+        slide[S].style.display="block";
+    } else if(S == 2) {
+        for (let i=0; i < slide.length; i++) {
+            desk[i].style.display="none";
+            slide[i].style.display="none";
+        }
+        S++;
+        desk[S].style.display="flex";
+        slide[S].style.display="block";
+    } else if(S == 3) {
+        for (let i=0; i < slide.length; i++) {
+            desk[i].style.display="none";
+            slide[i].style.display="none";
+        }
+        S++;
+        desk[S].style.display="flex";
+        slide[S].style.display="block";
+    } else if(S == 4) {
+        for (let i=0; i < slide.length; i++) {
+            desk[i].style.display="none";
+            slide[i].style.display="none";
+        }
+        S = 0;
+        desk[S].style.display="flex";
+        slide[S].style.display="block";
+    }
+
+    clearInterval(intervalID);
+    intervalID = setInterval(sliderAutomatic, 4000);
 }
